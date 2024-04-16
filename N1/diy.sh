a282b79c2020eef6e7220faf61a0910ec57d5211
jzqxx <126733798+jzqxx@users.noreply.github.com>
2024-04-16 11:11:13 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,8 +7,9 @@ sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_gener
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/lang/golang
 
 # Add packages
-git clone https://github.com/JohnstonIrene/My-Pkg package/my-pkg
+# git clone https://github.com/JohnstonIrene/My-Pkg package/my-pkg
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
