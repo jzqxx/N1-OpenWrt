c3dfd8619f24c2f9eaf9a392dd9d012c56226cb6
jzqxx <126733798+jzqxx@users.noreply.github.com>
2024-04-16 14:45:46 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_gener
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
 # Remove packages
-rm -rf feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
 
 # Add packages
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
