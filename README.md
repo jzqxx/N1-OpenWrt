2ad92f7bd7862a4c3d665b92d1cf4c20454774fd
jzqxx <126733798+jzqxx@users.noreply.github.com>
2026-03-17 10:07:40 +0800

    N1 OpenWrt

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # 项目简介
-本固件适配斐讯 N1 旁路由模式，追求轻量，不具备 PPPoE、WiFi 相关功能。<br>
-固件仅包含默认皮肤以及下列 luci-app：<br>
-[luci-app-amlogic](https://github.com/ophub/luci-app-amlogic)：系统更新、文件传输、CPU 调频等<br>
-[luci-app-passwall](https://github.com/Openwrt-Passwall/openwrt-passwall)：科学上网<br>
-[luci-app-podman](https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman)：容器管理<br>
-luci-app-samba4：存储共享
+本固件适配斐讯N1旁路由模式，追求轻量（请注意：不具备PPPoE、WiFi相关功能）。<br>
+固件包含默认皮肤、完整 IPv6 支持，以及下列 luci-app：<br>
+[luci-app-amlogic](https://github.com/ophub/luci-app-amlogic)：晶晨宝盒。<br>
+[luci-app-passwall](https://github.com/Openwrt-Passwall/openwrt-passwall)：科学上网。<br>
+[luci-app-openclash](https://github.com/vernesong/OpenClash): 科学上网。<br>
+[luci-app-mosdns](https://github.com/sbwml/luci-app-mosdns):  DNS转发器。<br>
+[luci-app-adguardhome](https://github.com/w9315273/luci-app-adguardhome):  广告拦截。<br>
+[luci-app-dockerman](https://github.com/lisaac/luci-app-dockerman)：容器管理。<br>
 ***
 # 致谢
-本项目基于 [ImmortalWrt-25.12](https://github.com/immortalwrt/immortalwrt/tree/openwrt-25.12) 源码编译，使用 flippy 的[脚本](https://github.com/unifreq/openwrt_packit)和 breakingbadboy 维护的[内核](https://github.com/breakingbadboy/OpenWrt/releases/tag/kernel_stable)打包成完整固件，感谢开发者们的无私分享。<br>
+本项目修改自nantayo的[N1-OpenWrt](https://github.com/nantayo/N1-OpenWrt)。<br>
+本项目基于 [ImmortalWrt-24.10](https://github.com/immortalwrt/immortalwrt/tree/openwrt-24.10) 源码编译，使用 flippy 的[脚本](https://github.com/unifreq/openwrt_packit)和 ophub 维护的[内核](https://github.com/ophub/kernel/releases/tag/kernel_stable)打包成完整固件，感谢开发者们的无私分享。<br>
 flippy 固件的更多细节参考[恩山论坛帖子](https://www.right.com.cn/forum/thread-4076037-1-1.html)。
--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,18 +3,25 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
-# Clone packages
-git clone https://github.com/nantayo/My-Pkg clone/my-pkg
+# Add packages
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
-git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
+git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
+git clone https://github.com/sbwml/luci-app-mosdns --single-branch --depth=1 clone/mosdns
+git clone https://github.com/sbwml/packages_lang_golang --single-branch --depth=1 clone/golang
+git clone https://github.com/sbwml/v2ray-geodata --depth=1 clone/v2ray-geodata
+git clone https://github.com/w9315273/luci-app-adguardhome --depth=1 package/luci-app-adguardhome
 
-# Adjust packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
-cp -rf clone/my-pkg/haproxy feeds/packages/net/
-cp -rf clone/my-pkg/podman feeds/packages/utils/
-sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
+# Update packages
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-openclash feeds/packages/lang/golang feeds/packages/net/mosdns feeds/packages/net/v2ray-geodata
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/openclash/luci-app-openclash clone/mosdns/luci-app-mosdns feeds/luci/applications/
+cp -rf clone/golang feeds/packages/lang/
+cp -rf clone/mosdns/mosdns clone/mosdns/v2dat clone/v2ray-geodata feeds/packages/net/
+sed -i 's/llvm=true/llvm=false/' feeds/packages/lang/rust/Makefile
 
 # Clean packages
 rm -rf clone
